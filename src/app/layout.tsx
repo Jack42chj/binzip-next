24c739fba30272c45a9fdb94bae03ba93981549c
Jack42chj <hojinch99@gmail.com>
2024-04-22 16:01:49 +0900

    Change Open Graph Image to png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ export const metadata: Metadata = {
         title: "BinZip | 유투브 국내 드라마 결말까지 몰아보자",
         description:
             "지나쳤던 드라마 몰아보자! 다양한 드라마 시작부터 결말까지 한번에!",
-        url: "http://binzip.vercel.app/",
+        url: "http://binzip.vercel.app",
         images: [
             {
-                url: "/webp/og_img.webp",
+                url: "/og/og_img.png",
                 alt: "국내 드라마 몰아보기 서비스",
             },
         ],
